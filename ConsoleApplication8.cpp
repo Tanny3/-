6f4d630ef484e58e6f9c259401316eed3bf35da2
Tanny3 <88582588+Tanny3@users.noreply.github.com>
2021-09-26 22:10:46 +0500

    Add files via upload

---
+++ ConsoleApplication8.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+void main()
+{
+	setlocale(LC_ALL, "rus");
+	int n1, n2;
+	std::cout << "Введите 2 числа\n";
+	std::cin >> n1 >> n2;
+	int i;
+	for (i = 1; i <= n1 && i <= n2; i++) {
+		if (n1%i == 0 && n2%i == 0)
+			std::cout <<"Остаток: " << i << "\n";
+	}
+
+}
