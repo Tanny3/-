a73c60d14a41c49031ce5b6082caafcf3db6b106
Tanny3 <88582588+Tanny3@users.noreply.github.com>
2021-09-26 21:58:38 +0500

    Add files via upload

---
+++ ConsoleApplication6.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+
+int main()
+{
+	int a{ 0 }, b = { 0 }, c = { 0 };
+	int count{ 0 };
+	for (int i = 100; i <= 999; i++)
+	{
+		a = i / 100;
+		b = (i / 10) % 10;
+		c = i % 10;
+
+		if (a != b && b != c && a != c)
+		{
+			count++;
+		}
+	}
+	std::cout << count;
+	return 0;
+}
---
+++ ConsoleApplication7.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int a, res;
+	double num = 0;
+	int i = 0;
+	std::cout << "Введите любое целое число: ";
+	std::cin >> a;
+	while (a != 0) {
+		res = a % 10;
+		if (res != 3 && res != 6) {
+			num += res * pow(10, i);
+			i++;
+		}
+		a /= 10;
+	}
+	std::cout << "Число без 3 и 6: " << num;
+    
+}
